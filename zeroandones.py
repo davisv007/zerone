b4d25bc2834bd1ce90046833e38546e9ee841a9e
davisv007 <davisv@berea.edu>
2017-09-27 16:30:47 -0400

    done for the day

--- zeroandones.py
+++ zeroandones.py
@@ -2,7 +2,7 @@ def main():
     intlist = list(input('zeroes and ones?'))
     state, prev, num, lenlist = None, None, 0, []
 
-    # group up ones with ones and zeroes with zeroes
+    # create list of lengths of consecutive zeroes and ones
     for element in intlist:
         state = element == '0'
         if prev != state:
@@ -11,14 +11,14 @@ def main():
         num += 1
         prev = state
 
-    # append final num,remove initial num
+    # append final num,remove initial 0
     lenlist = lenlist[1:] + [num]
 
-    pairlist = [lenlist[i: i + 2] for i in range(0, len(lenlist), 2)]
-    pairlist += [[lenlist[0]]] + [lenlist[i: i + 2] for i in range(1, len(lenlist), 2)] if len(lenlist) % 2 != 0 else []
+    pairlist = [lenlist[i: i + 2] for i in range(0, len(lenlist), 2)] + \
+               [lenlist[i: i + 2] for i in range(1, len(lenlist), 2)]
 
     # find the max across the mins across the pairs of the elements of these two lists
-    jax = max(list(map(min, pairlist)))
+    jax = max(list(map(min, [element for element in pairlist if len(element) == 2])))
     print(jax)
 
 
